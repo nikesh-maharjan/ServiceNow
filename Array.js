b7b4112c3eb0b09a0c83c8649609973028a2aa16
nikesh-maharjan <37381963+nikesh-maharjan@users.noreply.github.com>
2023-07-13 14:49:57 +0000

    msg

--- Array.js
+++ Array.js
@@ -1,19 +1,26 @@
 const paragraph = 'The quic quick brown fox jumps over the lazy dog. If the dog barked, was it really lazy?';
 
 // Any character that is not a word character or whitespace
-var t = "quicka";
-var u = new RegExp(t, "mi");
+var t = "quick";
+/**
+ * Regex
+ */
+// create regex with variable
+var u = new RegExp(t, "mi"); 
+// regex with variable does not work
 const regex = /t/g;
 
 console.log(paragraph.search(u));
-// Expected output: 43
+// Expected output: 9
 
 console.log(paragraph[paragraph.search(u)]);
-// Expected output: "."
+// Expected output: "q"
 
-console.log(paragraph.substring(9));
+console.log(paragraph.substring(paragraph.search(u)));
+// quick brown fox jumps over the lazy dog. If the dog barked, was it really lazy?
 
 var m = paragraph.match(u);
 console.log(m);
+
 
 console.log(null.toString());
