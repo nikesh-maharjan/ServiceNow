45a3bfdd840cb992b9434c6ec1332a5298b674ea
nikesh.cap@gmail.com <nikesh.cap@gmail.com>
2023-07-17 13:42:50 -0400

    ListExport

---
+++ UX Script/ExportList.js
@@ -0,0 +1,29 @@
+/**
+* @param {params} params
+* @param {api} params.api
+* @param {any} params.event
+* @param {any} params.imports
+* @param {ApiHelpers} params.helpers
+*/
+function handler({api, event, helpers, imports}) {
+    try {
+        /****** Fields that might need updating *****************/
+        const tableName = "tableName";
+        const viewName = "export";
+        const fixedFilter = api.data.get_user_group_query_all_1.output; // set by system
+        const listFilter = api.state.listFilter; // set by user
+        const selectedRows = api.state.selectedRows;
+        /********************************************************/
+        const {getListExportUrl} = imports["x_813128_aah.AFACTListUtils"]();
+        var url = getListExportUrl(tableName, viewName, fixedFilter, listFilter, selectedRows);
+        
+        // EMIT EVENT
+        api.emit('NAV_ITEM_SELECTED', {
+            external: {
+                "url": url
+            }
+        });
+    } catch (ex) {
+        // console.log(ex);
+    }
+}
