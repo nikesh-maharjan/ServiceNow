f4e5605043561f070ceb3788f4a289b593f6b394
nikesh-maharjan <37381963+nikesh-maharjan@users.noreply.github.com>
2023-07-17 12:15:42 -0400

    c1

---
+++ TestType.js
@@ -0,0 +1,11 @@
+//Daates
+console.log(typeof Number("42")); 
+// Expected output: "number"
+console.log(typeof new Number("42"));
+// Expected output: "object"
+console.log(typeof 42);
+// Expected output: "number"
+console.log(typeof parseInt("42", 10));
+// Expected output: "number"
+console.log(typeof undeclaredVariable);
+// Expected output: "undefined"
