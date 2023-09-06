5ddeb65636d215f5cd28c251a8624e31f39b3ca3
nikesh.cap@gmail.com <nikesh.cap@gmail.com>
2023-09-06 11:51:51 -0400

    color

---
+++ color.js
@@ -0,0 +1,62 @@
+"value": "* {font-size:18px;\r\nfont-weight: 900;\r\ntext-shadow: 2px 2px 5px rgb(211, 211, 211);\r\n    color: rgb(var(--now-color--primary-2)) }"
+
+"value": "* {color: rgb(100, 100, 100);\r\nfont-size: .85rem; \r\ntext-transform: capitalize;}"
+
+"background": "rgb(var(--now-color--neutral-1))"
+"color": "rgb(var(\"--now-color--primary-0\"))"
+
+
+
+
+    
+    //"--now-pill_identifier--background-color":"0,0,200",
+    //"--now-pill--background-color": "0,0,200",
+    // "--now-pill--color--selected": "0,0,200",
+
+    // "--now-pill--color--selected": "0,0,200",
+    // "--now-pill--background-color": "0,0,200",
+    // "--now-pill_identifier--background-color":"0,0,200",
+
+    "--now-pill--color--selected": "0,0,200",
+    "--now-pill--color--selected_active": "0,0,200",
+    "--now-pill--color--selected_hover": "0,0,200",
+    "--now-pill_identifier--background-color": "0,0,200",
+    "--now-pill--background-color": "0,0,200",
+
+
+    "--now-pill--background-color--active": "255, 255, 255",
+line 34
+"--now-canvas-header-container--background-color":"--now-color--primary-1",
+
+line 36
+"--now-canvas-menu--background-color":"--now-color--primary-1",
+
+line 40
+"--now-canvas-nav-item--main-nav--color--hover":"--now-color--primary-1",
+
+line 41
+"--now-canvas-nav-item--main-nav--color--active":"--now-color--primary-1",
+
+
+line 53,78,83,92,
+
+line 105
+"--now-button--bare_primary--color": "--now-color--primary-1",
+
+line 112
+"--now-button--bare_secondary--color": "--now-color--primary-1",
+
+line 180,
+
+line 210
+"--now-tabs--border-color--selected":"--now-color--primary-1",
+
+
+30,133,109
+"--now-color--primary-1": "255, 204, 1",
+--now-color--neutral-0
+
+
+
+
+    
